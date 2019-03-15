063cc6b76760327712d316e922788c59e8362bc2
REZA SAMSU RAMDANI <reza@student.ipb.ac.id>
2019-03-16 05:02:17 +0700

    create first component

---
+++ src/Component/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import styles from './App.component.style.js';
+
+class App extends Component {
+  render() {
+    return (
+        <h1 style={styles.something}>Its Working</h1>
+    );
+  }
+}
+
+export default App;
