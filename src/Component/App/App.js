f9f3afcfcec7df993f3463db70d81e65c9a998ff
REZA SAMSU RAMDANI <reza@student.ipb.ac.id>
2019-03-16 13:39:49 +0700

    display news from endpoint

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -1,33 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 import styles from './App.component.style.js';
+import Header from '../Header/Header.component.js';
 import NewsItem from '../NewsItem/NewsItem.component';
 
 class App extends Component {
     constructor(props){
       super(props);
       this.state = {
-        news: {
-            source: {
-                id: "the-new-york-times",
-                name: "The New York Times"
-            },
-            author: "DAVID LEONHARDT",
-            title: "The Experts Keep Getting the Economy Wrong",
-            description: "Traders at the New York Stock Exchange on Friday. The week ended on a down note after a disappointing jobs report.",
-            url: "https://www.nytimes.com/2019/03/10/opinion/us-economy-stagnation-growth.html",
-            urlToImage: "https://static01.nyt.com/images/2019/03/10/opinion/10leonhardt-promo/10leonhardt-promo-facebookJumbo.png",
-            publishedAt: "2019-03-10T23:14:57Z",
-            content: "Beside a lack of competition, the investment slump stems from what Summers calls the de-massification of the economy. Developers arent building as many malls and stores, because goods now go straight from warehouses to homes. Offices dont need as much storage… [+1924 chars]"
-        },
+        news: []
         }
       }
 
-      render(){
-      return (
-          <NewsItem news={this.state.news}/>
-      )
+      componentDidMount(){
+        axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=17d26abf61334f41836703a683fb2fb4')
+            .then((response) => {
+            this.setState({
+                news: response.data.articles
+            })
+        })
       }
+
+      render(){
+        return (
+           <div>
+               {this.state.news && this.state.news.map((news) => <NewsItem news={news}/>)}
+           </div>
+        )
     }
+}
 
 export default App;
