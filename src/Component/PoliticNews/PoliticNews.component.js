20d565a34106b6f245895b96a57df2830f0a6e9d
REZA SAMSU RAMDANI <reza@student.ipb.ac.id>
2019-03-16 16:23:28 +0700

    create category page

--- src/Component/PoliticNews/PoliticNews.component.js
+++ src/Component/PoliticNews/PoliticNews.component.js
@@ -1,3 +1,18 @@
 /**
  * Created by win7 on 16/03/2019.
  */
+import React, { Component } from 'react';
+
+import NewsList from '../NewsList/NewsList.component';
+import Header from '../Header/Header.component';
+
+export default class PoliticNews extends Component {
+    render(){
+        return (
+            <div>
+                <Header/>
+                <NewsList url="https://newsapi.org/v2/everything?q=politic&apiKey=17d26abf61334f41836703a683fb2fb4"/>
+            </div>
+        )
+    }
+}
--- src/Component/SocialNews/SocialNews.component.js
+++ src/Component/SocialNews/SocialNews.component.js
@@ -1,3 +1,18 @@
 /**
  * Created by win7 on 16/03/2019.
  */
+import React, { Component } from 'react';
+
+import NewsList from '../NewsList/NewsList.component';
+import Header from '../Header/Header.component';
+
+export default class SocialNews extends Component {
+    render(){
+        return (
+            <div>
+                <Header/>
+                <NewsList url="https://newsapi.org/v2/everything?q=social&apiKey=17d26abf61334f41836703a683fb2fb4"/>
+            </div>
+        )
+    }
+}
